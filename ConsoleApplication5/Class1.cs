f1987a182b4f5ea3e4b154619e69f86668517b6f
vov4ishka88 <vov4ishka1988@gmail.com>
2015-02-05 20:55:54 +0200

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ ConsoleApplication5/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApplication5
+{
+    class Class1
+    {
+        int a = 5;
+
+        public void printNumberOne() {
+
+            while (true) { 
+                Console.WriteLine("1 : " + a);                
+            Thread.Sleep(1000);
+            a++;
+            }
+        }
+
+        public void printNumberTwo() {
+
+            while (true)
+            {
+                Console.WriteLine("2 : " + a);
+                Thread.Sleep(1000);
+                a++;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new Class1();
+            Thread th1 = new Thread(new ThreadStart(obj1.printNumberOne));
+            Thread th2 = new Thread(new ThreadStart(obj1.printNumberTwo));
+            th1.Start();
+            th2.Start();
+
+        }
+    }
+}
